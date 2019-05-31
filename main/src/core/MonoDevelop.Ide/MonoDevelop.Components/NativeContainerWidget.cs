9c6cca658242447f3c97add638becb08a3f5b27c
Jose Medrano <jose.medrano@microsoft.com>
2019-05-31 17:23:24 +0200

    Encloses NativeContainerWidget into Mac conditionals

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components/NativeContainerWidget.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components/NativeContainerWidget.cs
@@ -26,6 +26,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+#if MAC
 
 using System;
 
@@ -75,3 +76,5 @@ namespace MonoDevelop.Components
 		}
 	}
 }
+
+#endif
